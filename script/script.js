94ac9a6889e7adbdf3987482ca71821fec910f8a
Glekshmi <lekshmi.g@techversantinfotech.com>
2024-01-24 17:27:59 +0530

    updates

--- script/script.js
+++ script/script.js
@@ -4,80 +4,87 @@ function validates() {
    let lblError;
    let regName = /^[a-zA-Z ]{2,30}$/;
    firstName = firstName.trim();
-   if (firstName == "")
+   if (firstName == "") {
       document.getElementById("lblError").innerHTML = "first name field cannot be blank!";
-   else if (!regName.test(firstName))
-      document.getElementById("lblError").innerHTML = "Enter a valid name";
-   else if (!(isNaN(firstName)))
+   } else if (!regName.test(firstName)) {
       document.getElementById("lblError").innerHTML = "name should contain alphabets only";
-   else
-      document.getElementById("lblError").innerHTML = " ";
+   } else if (!(isNaN(firstName))) {
+      document.getElementById("lblError").innerHTML = "Enter a valid name";
+   } else {
+      document.getElementById("lblError").innerHTML = "";
+   }
 
    let lastName;
    lastName = document.getElementById("lastName").value;
    let lblError1;
    lastName = lastName.trim();
-   if (lastName == "")
+   if (lastName == "") {
       document.getElementById("lblError1").innerHTML = "last name field cannot be blank!";
-   else if (!regName.test(lastName))
-      document.getElementById("lblError1").innerHTML = "Enter a valid name";
-   else if (!(isNaN(lastName)))
+   } else if (!regName.test(lastName)) {
       document.getElementById("lblError1").innerHTML = "name should contain alphabets only";
-   else
+   } else if (!(isNaN(lastName))) {
+      document.getElementById("lblError1").innerHTML = "Enter a valid name";
+   } else {
       document.getElementById("lblError1").innerHTML = "";
+   }
 
    let address;
    address = document.getElementById("address").value;
    let lblError2;
    address = address.trim();
-   if (address == "")
+   if (address == "") {
       document.getElementById("lblError2").innerHTML = "address field cannot be blank!";
-   else
+   } else {
       document.getElementById("lblError2").innerHTML = "";
+   }
 
    let phNo;
    phNo = document.getElementById("phNo").value;
    let lblError3;
    let maxLength = phNo.length;
-   if (phNo == "")
+   if (phNo == "") {
       document.getElementById("lblError3").innerHTML = "phone number field cannot be blank!";
-   else if (isNaN(phNo))
+   } else if (isNaN(phNo)) {
       document.getElementById("lblError3").innerHTML = "should contain digits only";
-   else if (maxLength < 10 || maxLength > 10)
+   } else if (maxLength < 10 || maxLength > 10) {
       document.getElementById("lblError3").innerHTML = "Number should contain atmost 10 digits";
-   else
+   } else {
       document.getElementById("lblError3").innerHTML = "";
+   }
 
    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
    let email;
    email = document.getElementById("email").value;
    let lblError4;
-   if (email == "")
+   if (email == "") {
       document.getElementById("lblError4").innerHTML = "email field cannot be blank!";
-   else if (!emailRegex.test(email))
+   } else if (!emailRegex.test(email)) {
       document.getElementById("lblError4").innerHTML = "invalid email id";
-   else
+   } else {
       document.getElementById("lblError4").innerHTML = "";
+   }
 
    let passw = /^[a-zA-Z0-9!@#$%^&*]$/;
    passWord = document.getElementById("passWord").value;
    let lblError5;
-   if (passWord == "")
+   if (passWord == "") {
       document.getElementById("lblError5").innerHTML = "password field cannot be blank!";
-   else if (passw.test(passWord))
+   } else if (passw.test(passWord)) {
       document.getElementById("lblError5").innerHTML = "input a strong password";
-   else if (passWord.length <= 8)
+   } else if (passWord.length <= 8) {
       document.getElementById("lblError5").innerHTML = "password should contain at least 8 characters";
-   else
+   } else {
       document.getElementById("lblError5").innerHTML = "";
-
-
-   confirmPassord = document.getElementById("confirmPassord").value;
+   }
+   let confirmPassord;
+   confirmPassord = document.getElementById("confirmPassword").value;
    let lblError6;
-   if (passWord == "")
+   if (passWord == "") {
       document.getElementById("lblError6").innerHTML = "password field cannot be blank!";
-   else if (confirmPassord == passWord)
+      return false;
+   } else if (confirmPassord == passWord) {
       document.getElementById("lblError6").innerHTML = "enter the same password as above";
-   else
+   } else {
       document.getElementById("lblError6").innerHTML = "";
+   }
 }
